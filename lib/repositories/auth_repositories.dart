23c431dca9418b910526407b5bc1cb82f1c23c71
dte-23010 <snayak@datopic.com>
2023-05-10 17:39:10 +0530

    change some code.

--- lib/repositories/auth_repositories.dart
+++ lib/repositories/auth_repositories.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:hive_with_bloc/hive/hive_database.dart';
+import 'package:hive_with_bloc/model/marital_list.dart';
 import 'package:hive_with_bloc/services/auth_services.dart';
-
-
 
 class AuthRepositories {
   final AuthServices authServices;
@@ -12,15 +12,20 @@ class AuthRepositories {
   });
   //patient login
 
-
-
   Future<List> maritalStatusList() async {
+    final box = await HiveDB().openFamilyStatusBox();
+    // For Iterate value and store inside list.
+    final listData = box.values.toList().cast<FamilyStatus>();
+    if (listData.isNotEmpty) {
+      print("##############33 hive execute");
+      box.close();
+      return listData;
+    }
+
     try {
       return await authServices.fetchMaritalList();
-    }  catch (e) {
+    } catch (e) {
       throw "Something wrong";
     }
   }
-
-  
 }
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -8,33 +8,25 @@ import 'package:http/http.dart' as http;
 class AuthServices {
   Future<List<FamilyStatus>> fetchMaritalList() async {
     final box = await HiveDB().openFamilyStatusBox();
-    // For Iterate value and store inside list.
-    final listData = box.values.toList().cast<FamilyStatus>();
+    try {
+      print("############## API execute");
+      await Future.delayed(Duration(seconds: 4));
+      const url = 'http://54.90.99.24:8000/api/v1/getMaritalStatusList';
+      final uri = Uri.parse(url);
+      final response = await http.get(uri);
 
-    if (listData.isNotEmpty) {
-      return listData;
-    } else {
-      // print("API Execute");
-      try {
-        await Future.delayed(Duration(seconds: 4));
-        const url = 'http://54.90.99.24:8000/api/v1/getMaritalStatusList';
-        final uri = Uri.parse(url);
-        final response = await http.get(uri);
-
-        if (response.statusCode == 200) {
-          final parsedJson = json.decode(response.body);
-          final list = parsedJson['family_status'] as List;
-          final value =
-              list.map((item) => FamilyStatus.fromJson(item)).toList();
-          await box.clear();
-          await box.addAll(value);
-          return value;
-        } else {
-          throw Exception('Failed to load marital status list');
-        }
-      } catch (e) {
-        throw Exception('Something went wrong');
+      if (response.statusCode == 200) {
+        final parsedJson = json.decode(response.body);
+        final list = parsedJson['family_status'] as List;
+        final value = list.map((item) => FamilyStatus.fromJson(item)).toList();
+        await box.clear();
+        await box.addAll(value);
+        return value;
+      } else {
+        throw Exception('Failed to load marital status list');
       }
+    } catch (e) {
+      throw Exception('Something went wrong');
     }
   }
 }
